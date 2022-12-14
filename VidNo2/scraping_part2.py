ccc35154528762813bd7e4393ef8445b9a87d3b9
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-12-15 01:24:31 +0200

    writes into changed html file

---
+++ VidNo2/scraping_part2.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup as bs
+import re  # regular expressions
+
+
+# Reading from dummyhtml_CourseRegistration.html file
+with open("../CourseRegistration.html", "r") as index:
+    doc = bs(index, "html.parser")
+
+
+# limit finds to 10 objects
+# tags = doc.find_all(text=re.compile("\$.*"), limit=10)
+
+# find input tags with type = text
+tags = doc.find_all("input", type="text")
+# value = tag["selected"]= "false"
+# tag['color'] = "black"
+# for tag in tags:
+#     print(tag.strip())      # strip and remove spaces
+# print(tags)
+# print(tag.attrs)
+
+
+for tag in tags:
+    tag['placeholder'] = "I changed you!"
+    # print(type(tag))
+
+with open("v2CourseRegistration.html", "w") as file:
+    file.write(str(doc))
