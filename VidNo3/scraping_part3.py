2b6505e33763c863218876da37f25a472f580dc2
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-12-17 04:56:09 +0200

    change coinName Variable

--- VidNo3/scraping_part3.py
+++ VidNo3/scraping_part3.py
@@ -13,24 +13,31 @@ TreeBody = document.tbody
 
 tableRows = TreeBody.contents
 
+# print(tableRows)
+
 # grap next sibling to the first element in table row
 # print(tableRows[0].next_siblings)
 # print(list(tableRows[0].next_siblings))
 # print(tableRows[1].previous_sibling)
 # print(tableRows[0].parent.name) # Can use a list with it, List()
+# # print(list(tableRows[0].contents))
+# # you can replace the contents attr with children
 # print(list(tableRows[0].contents))
-# you can replace the contents attr with children
-# print(list(tableRows[0].contents))
 
 
-prices = {}
+prices = {}  # contains pair of name : price
+
+# for iterator in tableRows:
+#    for td in iterator.contents:
+#       print(td)
+#       print()
 
 
-for tr in tableRows[:10]:
-   name, price = tr.contents[2:4]
-   fixed_name = name.p.string
-   fixed_price = price.a.string
+for iterator in tableRows[:10]:
+   coin, price = iterator.contents[2:4]
+   coinName = coin.p.string
+   coinPrice = price.a.string
+   prices[coinName] = coinPrice  # giving each pair of {coin : price}
 
-   prices[fixed_name] = fixed_price
 
 print(prices)
